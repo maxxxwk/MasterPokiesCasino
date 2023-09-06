80a646732cba173bf14d45003124c8329910ba8a
maksym.vakoniuk <makswk04@gmail.com>
2023-09-06 20:05:46 +0300

    Fixed webview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,6 @@ dependencies {
     implementation("com.github.leonard-palm:compose-state-events:2.0.3")
 
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    implementation("com.google.accompanist:accompanist-webview:0.30.1")
 }
--- app/src/main/java/com/upstars/masterpokiescasino/screens/webview/presentation/WebviewScreen.kt
+++ app/src/main/java/com/upstars/masterpokiescasino/screens/webview/presentation/WebviewScreen.kt
@@ -1,36 +1,26 @@
 package com.upstars.masterpokiescasino.screens.webview.presentation
 
 import android.annotation.SuppressLint
-import android.view.ViewGroup
 import android.webkit.CookieManager
 import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun WebviewScreen(viewModel: WebviewScreenViewModel) {
-    AndroidView(
+    WebView(
         modifier = Modifier.fillMaxSize(),
-        factory = {
-            WebView(it).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                webViewClient = WebViewClient()
-                settings.javaScriptEnabled = true
-                settings.domStorageEnabled = true
-                settings.cacheMode = WebSettings.LOAD_DEFAULT
-                CookieManager.getInstance().setAcceptCookie(true)
-                CookieManager.getInstance().setAcceptThirdPartyCookies(this, true)
-                loadUrl(viewModel.getURL())
-            }
-        },
-        update = { it.loadUrl(viewModel.getURL()) }
+        state = rememberWebViewState(url = viewModel.getURL()),
+        onCreated = { webview ->
+            webview.settings.javaScriptEnabled = true
+            webview.settings.domStorageEnabled = true
+            webview.settings.cacheMode = WebSettings.LOAD_DEFAULT
+            CookieManager.getInstance().setAcceptCookie(true)
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webview, true)
+        }
     )
 }
